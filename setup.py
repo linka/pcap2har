895863fdd55261f21f10ffe44edcc50c6d7a397f
Elina Suslova <ellin@yandex-team.ru>
2012-08-08 16:32:21 +0300

    Added setup file

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(
+	name = "pcap2har",
+	version = "0.1",
+	description = "Converts .pcap network capture files to HTTP Archive files",
+	author = "Elina Suslova",
+	author_email = "ellin@yandex-team.ru",
+	packages = find_packages()
+)
